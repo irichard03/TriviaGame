1bdc138e6cece4ecc9e8eedbb7ce7e3d8be4b34a
Ian Richard <ianrichard03@gmail.com>
2018-09-13 22:06:19 -0500

    Created TriviaGame object, question object, and question constructor method.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,14 +4,51 @@
  * 
  */
 
+//Hey Trivia game the object.
+var triviaGame = {
+    numQuestions = 0,
+    rightAnswers = 0,
+    wrongAnswer = 0,
+    
+    //questions the object within trivia game
+    questions = { 
+        question = "why am I here?",
+        rightAnswer = "You got someplace better to be?",
+        rightNumber = 0,
+        wrongAnswers = ["Yes", "No", "True", "False"],
+        
+        Question: function(){
+            this.question = ask,
+            this.rightanswer = answer,
+            this.wrongAnswers = nope
+        },
+
+        shuffle : function(){
+            let x = Math.floor(Math.random() * 4) + 1;
+            this.rightNumber = x;
+       }
+    }
+    
+}
+//Constructor for question
+function Question(ask,answer,nope) {
+    
+//constructor for quiz
+function TriviaGame(count,right,wrong,)
+
+
+
+var shazBot = "shazbot";
+
  //Java script will execute this functionafter the page has loaded, launching the program.
-$(document).ready(function(){
-    var x = "shazbot";
+ $(document).ready(function(){
+    
+    
+    
+    
     
     debugMe(x);
 
-
-    
     //override for debug me taking a variable parameter.
     function debugMe(shazBot){
         console.log("This " + shazBot + " happened!");
