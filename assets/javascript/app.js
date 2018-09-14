eb1c1867777b0240fcfe4d886cd510e72ba8e47f
Ian Richard <ianrichard03@gmail.com>
2018-09-13 23:47:44 -0500

    removed methods and nested objects.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,61 +6,52 @@
 
 //Trivia game the object.
 var triviaGame = {
-    numQuestions = 0,
-    rightAnswers = 0,
-    wrongAnswers = 0,
-    
-    //game object defines the questions, answers, wrong answers, and a method-
-    // to replace a random wrong answer with the correct answer, and a constructor to create questions.
-    //mostly I just want to try out nested objects.
-    game = { 
-        question = "why am I here?",
-        rightAnswer = "You got someplace better to be?",
-        rightNumber = 0,
-        allAnswers = ["Yes", "No", "True", "False"],
-        
-        //Game CONSTRUCTOR METHOD.
-        Game: function(ask, answer, nope){
-            
-            this.question = ask,
-            this.rightanswer = answer,
-            this.allAnswers = nope
-        },
-
-        //Shuffle METHOD will get a random number from 1 to 4 and ovewrite one of the wrong answers in all answers
-        //array using the array method splice
-        shuffle : function(){
-            
-            this.rightNumber = Math.floor(Math.random() * 4) + 1;
-            this.allAnswers.splice(x,0,this.rightAnswer);
-       }
-    },
-
-    //triviaGame constructor  fixed version (not DRY)
-    TriviaGame : function(quiz,right,wrong){
-        this.numQuestions = quiz;
-        this.rightAnswers = right;
-        this.wrongAnswers = wrong;
-        var myGame = new Game( "What is your favorite color?", "red", ["blue, Green, Purple, Orange"] );
-        console.log(myGame.allAnswers);
-        myGame.shuffle();
-        console.log(myGame.allAnswers);        
-    }
-    
+    numQuestions : 0,
+    rightAnswers : 0,
+    wrongAnswers : 0
+}
+//game object defines the questions, answers, wrong answers, and a method-
+var game = { 
+        question : "why am I here?",
+        rightAnswer : "You got someplace better to be?",
+        rightNumber : 0,
+        allAnswers : ["Yes", "No", "True", "False"]
 }
 
+function TriviaGame(quiz,right,wrong){
+    this.numQuestions = quiz;
+    this.rightAnswers = right;
+    this.wrongAnswers = wrong;   
+}
 
+//Game CONSTRUCTOR METHOD.
+function Game(ask, answer, nope){
+    this.question = ask;
+    this.rightanswer = answer;
+    this.allAnswers = nope;
+}
+
+//Shuffle METHOD will get a random number from 1 to 4 and ovewrite one of the wrong answers in all answers
+function shuffle(gameObject){
+    myGame = gameObject;
+    myGame.rightNumber = Math.floor(Math.random() * 4) + 1;
+    return myGame.allAnswers = myGame.allAnswers.splice(myGame.rightNumber,0,this.rightAnswer);
+}
 
 var shazBot = "shazbot";
 
  //Java script will execute this functionafter the page has loaded, launching the program.
  $(document).ready(function(){
     
+    var myTriviaGame = new TriviaGame(3,0,0);
+    var myGame = new Game( "What is your favorite color?", "red", ["blue, Green, Purple, Orange"] );    //this will need to change for reusability
+    console.log(myGame.allAnswers);
+    myGame.allAnswers = shuffle(myGame);        
+    console.log(myGame.allAnswers + " after game shuffle");
     
-    var newGame = new TriviaGame(3,0,0,first)
     
     
-    debugMe(x);
+    //debugMe(x);
 
     //override for debug me taking a variable parameter.
     function debugMe(shazBot){
