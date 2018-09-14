fcd27b848ecda64e936c305c02819a4d049058fe
Ian Richard <ianrichard03@gmail.com>
2018-09-14 01:45:19 -0500

    redid game constructor randomly picks number and inserts correct answer into array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,39 +4,25 @@
  * 
  */
 
-//Trivia game the object.
+//Declare a Trivia game object and initialize with default values.
 var triviaGame = {
     numQuestions : 0,
     rightAnswers : 0,
-    wrongAnswers : 0
-}
-//game object defines the questions, answers, wrong answers, and a method-
+    wrongAnswers : 0,
+};
+//Declare a game object and initialize it with default valuse.  It defines the questions, answers, and wrong answers.
 var game = { 
         question : "why am I here?",
-        rightAnswer : "You got someplace better to be?",
+        correctAnswer : "You got someplace better to be?",
         rightNumber : 0,
-        allAnswers : ["Yes", "No", "True", "False"]
-}
+        allAnswers : ["Yes", "No", "True", "False"],
 
-function TriviaGame(quiz,right,wrong){
-    this.numQuestions = quiz;
-    this.rightAnswers = right;
-    this.wrongAnswers = wrong;   
-}
+};
 
-//Game CONSTRUCTOR METHOD.
-function Game(ask, answer, nope){
-    this.question = ask;
-    this.rightanswer = answer;
-    this.allAnswers = nope;
-}
+
 
 //Shuffle METHOD will get a random number from 1 to 4 and ovewrite one of the wrong answers in all answers
-function shuffle(gameObject){
-    myGame = gameObject;
-    myGame.rightNumber = Math.floor(Math.random() * 4) + 1;
-    return myGame.allAnswers = myGame.allAnswers.splice(myGame.rightNumber,0,this.rightAnswer);
-}
+
 
 var shazBot = "shazbot";
 
@@ -44,12 +30,32 @@ var shazBot = "shazbot";
  $(document).ready(function(){
     
     var myTriviaGame = new TriviaGame(3,0,0);
-    var myGame = new Game( "What is your favorite color?", "red", ["blue, Green, Purple, Orange"] );    //this will need to change for reusability
-    console.log(myGame.allAnswers);
-    myGame.allAnswers = shuffle(myGame);        
-    console.log(myGame.allAnswers + " after game shuffle");
+    var myGame = new Game( "What is your favorite color?", "red", ["blue", "Green", "Purple", "Orange"] );    //this will need to change for reusability
+    var myGame2 = new Game( "How many fingers am I holding up?", "One.", ["Two","Three","Four","Five"] );
+   // console.log(myGame.allAnswers[x]);
     
     
+
+    //Construct trivia game.
+function TriviaGame(quiz,right,wrong){
+    this.numQuestions = quiz;
+    this.rightAnswers = right;
+    this.wrongAnswers = wrong;   
+}
+
+//Game CONSTRUCTOR METHOD, randomizes answer location.
+function Game(ask, answer, choices){
+    this.question = ask;
+    this.correctAnswer = answer;
+    this.allAnswers = choices;
+    this.rightNumber = Math.floor(Math.random() * 4) +1;
+    this.allAnswers.splice(this.rightNumber - 1 , 1 ,this.correctAnswer);   //replace random element in array with correct answer.
+    console.log(this.rightNumber);
+    console.log(this.correctAnswer);
+    console.log(this.allAnswers);
+    
+   
+}
     
     //debugMe(x);
 
