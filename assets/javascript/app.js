d43cc8e97c30cbf2950efbc24e0e9b2be10215e5
Ian Richard <ianrichard03@gmail.com>
2018-09-15 17:59:17 -0500

    added timer object, answer html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,5 @@
 /**
- * 
  *  Trivia Game by Ian Richard 
- * 
  */
 
 //Declare a Trivia game object and initialize with default values.
@@ -10,6 +8,42 @@ var triviaGame = {
     rightAnswers : 0,
     wrongAnswers : 0,
 };
+
+var timeLord = {
+    timeOver : 10000,    //holds time limit in miliseconds.
+    playTime: 4,        //used to set play time for rewards/taunts.
+    clock : 0,
+    counter: null,
+
+
+    countDown : function(){
+        if(this.clock <= 0){
+            timeLord.stopTimer();
+            $(".timer").text("Time's up!");
+            $("[answer='correct']").css("background-color", "red");
+        }
+        else{
+            $(".timer").text(timeLord.clock);
+            timeLord.clock--;
+            console.log(timeLord.clock);
+        }
+        
+
+    },
+
+    startTimer : function(){
+         this.counter = setInterval(this.countDown, 1000);    
+
+
+    },
+
+    stopTimer : function(){
+        clearInterval(this.counter);
+    }
+}
+        
+
+
 //Declare a game object and initialize it with default valuse.  It defines the questions, answers, and wrong answers.
 var game = { 
         question : "why am I here?",
@@ -19,49 +53,51 @@ var game = {
 
 };
 
-
-
-//Shuffle METHOD will get a random number from 1 to 4 and ovewrite one of the wrong answers in all answers
-
-
 var shazBot = "shazbot";
-
- //Java script will execute this functionafter the page has loaded, launching the program.
- $(document).ready(function(){
-    
+//Java script will execute this functionafter the page has loaded, launching the program.
+ 
+$(document).ready(function(){
     var myTriviaGame = new TriviaGame(3,0,0);
-    var myGame = new Game( "What is your favorite color?", "red", ["blue", "Green", "Purple", "Orange"] );    //this will need to change for reusability
-    var myGame2 = new Game( "How many fingers am I holding up?", "One.", ["Two","Three","Four","Five"] );
-   // console.log(myGame.allAnswers[x]);
+    var qustionOne = new Game( "What is your favorite color?", "red", ["blue", "Green", "Purple", "Orange"] );    //this will need to change for reusability
+    var questionTwo = new Game( "How many fingers am I holding up?", "One.", ["Two","Three","Four","Five"] );
+    var QuestionThree = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
+    var QuestionFour = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
+    var QuestionFive = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
     
-    
+    timeLord.startTimer();
 
-    //Construct trivia game.
-function TriviaGame(quiz,right,wrong){
-    this.numQuestions = quiz;
-    this.rightAnswers = right;
-    this.wrongAnswers = wrong;   
-}
+
+    //timer();
+
+    // console.log(myGame.allAnswers[x]);
+
+//Construct trivia game.
+    function TriviaGame(quiz,right,wrong){
+        this.numQuestions = quiz;
+        this.rightAnswers = right;
+        this.wrongAnswers = wrong;   
+    }
 
 //Game CONSTRUCTOR METHOD, randomizes answer location.
-function Game(ask, answer, choices){
-    this.question = ask;
-    this.correctAnswer = answer;
-    this.allAnswers = choices;
-    this.rightNumber = Math.floor(Math.random() * 4) +1;
-    this.allAnswers.splice(this.rightNumber - 1 , 1 ,this.correctAnswer);   //replace random element in array with correct answer.
-    console.log(this.rightNumber);
-    console.log(this.correctAnswer);
-    console.log(this.allAnswers);
+    function Game(ask, answer, choices){
+        this.question = ask;
+        this.correctAnswer = answer;
+        this.allAnswers = choices;
+        this.rightNumber = Math.floor(Math.random() * 4) +1;
+        this.allAnswers.splice(this.rightNumber - 1 , 1 ,this.correctAnswer);   //replace random element in array with correct answer.
+        console.log(this.rightNumber);
+        console.log(this.correctAnswer);
+        console.log(this.allAnswers);     
+    }
     
-   
-}
-    
-    //debugMe(x);
+    //function to call the timer.
 
-    //override for debug me taking a variable parameter.
+
+    //function for the counter.
+   /// function counter(){
+
     function debugMe(shazBot){
-        console.log("This " + shazBot + " happened!");
+            console.log("This " + shazBot + " happened!");
     }
 
 });
