2bb13872a463cfb22f593942d2bfc25c2f2ef796
Ian Richard <ianrichard03@gmail.com>
2018-09-15 23:08:15 -0500

    added switch for populating questions using template literal and html classnames.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,68 +6,121 @@
 
 //Java script will execute this functionafter the page has loaded, launching the program.
 $(document).ready(function(){
-    var triviaGame = {
-        numQuestions : 0,
-        rightAnswers : 0,
-        wrongAnswers : 0,
-    }
-    var controller;    
-    var clock;
-    var player;
-
-    //Declare a game object and initialize it with default valuse.  It defines the questions, answers, and wrong answers.
-    var game = { 
-            question : "why am I here?",
-            correctAnswer : "You got someplace better to be?",
-            rightNumber : 0,
-            allAnswers : ["Yes", "No", "True", "False"],
-            activeQuestion: false,
-    }
-               
-    var myTriviaGame = new TriviaGame(3,0,0);
-    var questionOne = new Game( "What is your favorite color?", "red", ["blue", "Green", "Purple", "Orange"] );    //this will need to change for reusability
-    var questionTwo = new Game( "How many fingers am I holding up?", "One.", ["Two","Three","Four","Five"] );
-    var QuestionThree = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
-    var QuestionFour = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
-    var QuestionFive = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
     
-    startTimer();
+    startgame();
 
+    function startgame(){
 
-    function countDown(){
-        if(clock === 0){
-            clearInterval(controller);
+    
+        var triviaGame = {
+            numQuestions : 0,
+            rightAnswers : 0,
+            wrongAnswers : 0,
         }
-        else{
-        $("timer").text(clock);
-        clock--;
-        console.log(clock);
+        var controller;    
+        var clock;
+        var player;
+
+        //Declare a game object and initialize it with default valuse.  It defines the questions, answers, and wrong answers.
+        var game = { 
+                question : "why am I here?",
+                correctAnswer : "You got someplace better to be?",
+                rightNumber : 0,
+                allAnswers : ["Yes", "No", "True", "False"],
+                activeQuestion : false,
+                id : "default"
         }
-    }
+                
+        var myTriviaGame = new TriviaGame(5,0,0);
+        var questionOne; 
+        var questionTwo; 
+        var questionThree; 
+        var QuestionFour = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
+        var QuestionFive = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
 
-    function startTimer(){
-        clock = 10;
-        player = 4;
-        controller = setInterval(countDown,1000);
-    }
+        
+            pickQuestion();
+       
 
-//Construct trivia game.
-    function TriviaGame(quiz,right,wrong){
-        this.numQuestions = quiz;
-        this.rightAnswers = right;
-        this.wrongAnswers = wrong;   
-    }
 
-//Game CONSTRUCTOR METHOD, randomizes answer location.
-    function Game(ask, answer, choices){
-        this.question = ask;
-        this.correctAnswer = answer;
-        this.allAnswers = choices;
-        this.rightNumber = Math.floor(Math.random() * 4) +1;
-        this.allAnswers.splice(this.rightNumber - 1 , 1 ,this.correctAnswer);   //replace random element in array with correct answer.
-        console.log(this.rightNumber);
-        console.log(this.correctAnswer);
-        console.log(this.allAnswers);     
+        function pickQuestion(){
+            
+            switch(myTriviaGame.numQuestions) {
+                case 5:
+                    questionOne = new Game( "How many fingers am I holding up?", "One", ["Two","Three","Four","Five"] );
+                    drawQuestion(questionOne);
+                    break;
+                case 4:
+                    questionTwo = new Game( "How many fingers am I holding up?", "One", ["Two","Three","Four","Five"] );
+                    drawQuestion(questionTwo);
+                    break;
+                case 3:
+                    questionThree = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
+                    drawQuestion(questionThree);
+                    break;
+                case 2:
+                    questionFour = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
+                    drawQuestion(questionFour);
+                    break;
+                case 2:
+                    questionFive = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
+                    draQuestion(questionFive);
+                    break;
+            }
+        }
+
+        function drawQuestion(x){
+            $(".question").text(x.question);
+            for(var y = 0; y <= 3; y++){
+                $(`.answer${y}`).text(x.allAnswers[y]);
+                console.log(y);
+                console.log(x.allAnswers[y]);
+            }
+            
+                    
+        }
+        
+
+        function countDown(){
+            if(clock === 0){
+                clearInterval(controller);
+                $(".timer").text("Time's up!");
+                setTimeout(function(){
+                    alert("Blaah!");            //call next
+                },3000)
+            }
+            else{
+            $(".timer").text(clock);
+            clock--;
+            //console.log(clock);
+            }
+        }
+
+        function startTimer(){
+            clock = 10;
+            player = 4;
+            controller = setInterval(countDown,1000);
+        }
+
+    //Construct trivia game.
+        function TriviaGame(quiz,right,wrong){
+            this.numQuestions = quiz;
+            this.rightAnswers = right;
+            this.wrongAnswers = wrong;   
+        }
+
+    //Game CONSTRUCTOR METHOD, randomizes answer location.
+        function Game(ask, answer, choices){
+            this.question = ask;
+            this.correctAnswer = answer;
+            this.allAnswers = choices;
+            this.activeQuestion = true;
+            this.rightNumber = Math.floor(Math.random() * 4) +1;
+            this.allAnswers.splice(this.rightNumber - 1 , 1 ,this.correctAnswer);   //replace random element in array with correct answer.
+           // console.log(this.rightNumber);
+           // console.log(this.correctAnswer);
+            //console.log(this.allAnswers);     
+        }
     }
 });
 
