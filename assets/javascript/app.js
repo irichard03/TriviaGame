238e5be658b8279b7f87258d35cb7c41fed5e10f
Ian Richard <ianrichard03@gmail.com>
2018-09-16 12:50:31 -0500

    added if else to stop game after cycling through questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,11 +32,11 @@ $(document).ready(function(){
         }
                 
         var myTriviaGame = new TriviaGame(5,0,0);
-        var question1 = new Game( "How many fingers am I holding up?", "One", ["Two","Three","Four","Five"] );
-        var question2= new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
-        var question3 = new Game( "How many fingers am I holding up?", "One", ["Two","Three","Four","Five"] );
-        var question4 = new Game( "Which movie was Bruce Campbell not in?", "Terminator", ["Army of Darkness","Spiderman","Evil Dead","Bubbahotep"] );
-        var question5 =  new Game( "How many fingers am I holding up?", "One", ["Two","Three","Four","Five"] );
+        var question1 = new Game( "What is the title of Bruce Campbell's first book?", "If Chins could Kill", ["Bruce Campbell vs. Hollywood","Real Confessions of the Briscoe Kid","Man with the Screaming Brain","Make Love the Bruce Campbell Way"] );
+        var question2= new Game( "Which movie was Bruce Campbell not in?", "The Punisher", ["Army of Darkness","Spider-man","Fargo","Escape from L.A."] );
+        var question3 = new Game( "Which television series has Bruce Cambell not appeared on?", "Star Trek The Next Generation", ["X-Files","Fargo","Xena","Burn Notice"] );
+        var question4 = new Game( "Complete the phrase: Klaatu, Barada, ", "Nikto", ["Necktie","Nickel","Noodle","Nectar"] );
+        var question5 =  new Game( "What was the character name of the Bruce Campbell's re-ocurring role on Hercules?", "Autolycus", ["Thesues","Heroditus","Shemp","Polyphemus"] );
         var quizArray = [question1, question2, question3, question4, question5];
 
         
@@ -55,13 +55,25 @@ $(document).ready(function(){
                             alert("Game over");
                         }
                         else{                                  
-                            drawQuestion(quizArray[myTriviaGame.numQuestions]);    //otherwise draw next question
+                            myTriviaGame.numQuestions--;
+                            setTimeout(function(){
+                                drawQuestion(quizArray[myTriviaGame.numQuestions-1]);
+                              },3000)
                         }                           
                     });
                 }                
                 else{
                     $( `.answer${y}` ).click(function(y){       //code executed on incorrect response
-                        stopTimer();                            
+                        myTriviaGame.numQuestions--;
+                        if(myTriviaGame.numQuestions === 0){
+                            alert("Game over");
+                        }
+                        else{
+                            setTimeout(function(){
+                                drawQuestion(quizArray[myTriviaGame.numQuestions-1]);
+                            },3000)
+                            stopTimer();                            
+                        }
                     }); 
                     
                 }
@@ -79,9 +91,15 @@ $(document).ready(function(){
             if(clock === 0){
                 clearInterval(controller);
                 $(".timer").text("Time's up!");
-                setTimeout(function(){
-                  //  alert("Blaah!");            //call next
-                },3000)
+                myTriviaGame.numQuestions--;
+                if(myTriviaGame.numQuestions === 0){
+                    alert("Game over");
+                }
+                else{
+                    setTimeout(function(){
+                        drawQuestion(quizArray[myTriviaGame.numQuestions-1]);
+                    },3000)
+                }
             }
             else{
             $(".timer").text(clock);
@@ -93,7 +111,7 @@ $(document).ready(function(){
         function startTimer(){
             clock = 10;
             player = 4;
-            controller = setInterval(countDown,1000);
+            controller = setInterval(countDown,500);
         }
 
         function stopTimer(){
