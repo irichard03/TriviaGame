2967a6b48ab08da4e34fcd59e0cfaec7dca5a8a5
Ian Richard <ianrichard03@gmail.com>
2018-09-16 21:33:07 -0500

    moved right picks and wrong picks to separate functions broke result display.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,17 +11,6 @@ $(document).ready(function(){
 
     function startgame(){
 
-    
-        var triviaGame = {
-            numQuestions : 0,
-            rightAnswers : 0,
-            wrongAnswers : 0,
-        }
-        var controller;    
-        var clock;
-        var step = 0;
-        
-
         //Declare a game object and initialize it with default valuse.  It defines the questions, answers, and wrong answers.
         var game = { 
                 question : "why am I here?",
@@ -32,17 +21,29 @@ $(document).ready(function(){
                 id : "default"
         }
                 
-        var myTriviaGame = new TriviaGame(5,0,0);
+        //Question objects, store the Question, the correct answer, and an array of incorrect answers. When the constructor is called
+        //It replaces one of the incorrect questions with the correct question randomly.
         var question1 = new Game( "What is the title of Bruce Campbell's first book?", "If Chins could Kill", ["Bruce Campbell vs. Hollywood","Real Confessions of the Briscoe Kid","Man with the Screaming Brain","Make Love the Bruce Campbell Way"] );
         var question2= new Game( "Which movie was Bruce Campbell not in?", "The Punisher", ["Army of Darkness","Spider-man","Fargo","Escape from L.A."] );
         var question3 = new Game( "Which television series has Bruce Cambell not appeared on?", "Star Trek The Next Generation", ["X-Files","Fargo","Xena","Burn Notice"] );
         var question4 = new Game( "Complete the phrase: Klaatu, Barada, ", "Nikto", ["Necktie","Nickel","Noodle","Nectar"] );
         var question5 =  new Game( "What was the character name of the Bruce Campbell's re-ocurring role on Hercules?", "Autolycus", ["Thesues","Heroditus","Shemp","Polyphemus"] );
-        var question6 =  new Game( "What was the character name of the Bruce Campbell's re-ocurring role on Hercules?", "Autolycus", ["Thesues","Heroditus","Shemp","Polyphemus"] );
-        var quizArray = [question1, question2, question3, question4, question5,question6];
+        var quizArray = [question1, question2, question3, question4, question5];
 
+        var numQuestions = 5;   //number of questions to be asked, variable to control which array element to pull.
+        var rightAnswers = 0;
+        var wrongAnswers = 0;
         
-        drawQuestion(quizArray[myTriviaGame.numQuestions-1]);
+        var controller;         //vaiable to store setInterval
+        var clock;              //stores timer value 0-10.
+        var step = 1;           //stores current question, when = 5 quiz ends.
+
+        //console.log(numQuestions);            debugging
+        //console.log(rightAnswers);
+        //console.log(wrongAnswers);
+        //console.log(step);
+
+        drawQuestion(quizArray[step-1]);
         //Fills the Question Div with the question from game object.
         function drawQuestion(x){
             $(".question").text(x.question);
@@ -63,85 +64,110 @@ $(document).ready(function(){
                 });
             }//end of loop
             
+            //start timer
             startTimer();
-            
+            console.log("Step is ( after start timer): " + step);
+            console.log("numQuestions is (after start timer): " + numQuestions);
+
             $(".answer[hint='RIGHT']").click(function(){        //code executed on right response
-                    stopTimer();
-                if(step === 5){
-                    myTriviaGame.rightAnswers++;
-                    alert("Game over");
-                    
-                }
-                else{                                  
-                    stopTimer();
-                    myTriviaGame.rightAnswers++;
-                    myTriviaGame.numQuestions--;
-                    setTimeout(function(){
-                        drawQuestion(quizArray[myTriviaGame.numQuestions-1]);
-                    },3000)
-                        
-                }                           
+                rightAnswer();        
             });
 
             $(".answer[hint='WRONG']").click(function(){       //code executed on incorrect response
-                stopTimer();
-                if(step === 5){
-                    myTriviaGame.wrongAnswers++;
-                    alert("Game over");
-                }
-                else{
-                    stopTimer();
-                    myTriviaGame.wrongAnswers++;
-                    myTriviaGame.numQuestions--;
-                    $("#displayMe").empty
-                    setTimeout(function(){
-                        drawQuestion(quizArray[myTriviaGame.numQuestions-1]);
-                    },3000)
-                                                    
-                    }
+                wrongAnswers();
             }); 
             
 
 
         }
 
+        function wrongAnswer(){
+            stopTimer();
+            wrongAnswers++;
+            if(step === 5){
+                alert("Game over");
+            }
+            else{
+                //  stopTimer();
+                wrongAnswers++;
+                step++;
+                setTimeout(function(){
+                    drawQuestion(quizArray[step]);
+                    },3000)
+                                                    
+                }
+        }
+
+
+        function rightAnswer(){
+            stopTimer();
+            rightAnswers++;
+            if(step === 5){
+                alert("Game over");
+                
+            }
+            else{                                  
+                //stopTimer();
+                step++;
+                console.log()
+                setTimeout(function(){
+                    drawQuestion(quizArray[step]);
+                },3000)
+                    
+            }       
+
+        }
+
+        function gameOver(){
+            $("#displayMe").empty();
+            $(".question").text("Quiz Over.");
+            $("#answer0").text("Right Answers: " + rightAnswers + " ");
+            $("#answer1").text("Wrong Answers: " + wrongAnswers + " ");
+            $("#answer2").empty();
+            $("#answer3").empty();
+            $("#timer").empty();
+
+        }
+
         function countDown(){
             
-
             if((clock === 0) && (step === 5)){
                 clearInterval(controller);
                 $(".timer").text("Time's up!");
-                alert("Game over");
-                    
-            }else if(clock === 0){
+                gameOver();
+
+            }else if((clock === 0) && (step != 5)){
                 clearInterval(controller);
-                myTriviaGame.numQuestions--;
+                step++;
                 setTimeout(function(){
-                    drawQuestion(quizArray[myTriviaGame.numQuestions-1])
+                    drawQuestion(quizArray[step])
                 },3000);
             }
             
-            $(".timer").text(clock);
-            clock--;
+            else{
+                $(".timer").text(clock);
+                clock--;
+                if(clock <= 0){
+                    clearInterval(controller);
+                    step++;
+                    setTimeout(function(){
+                        drawQuestion(quizArray[step])       //wait three seconds then display next answer.
+                    },3000);
+                }
+            }
     
         }
 
         function startTimer(){
             clock = 10;
             step++;
-            controller = setInterval(countDown,1000);
+            controller = setInterval(countDown,250);
         }
 
         function stopTimer(){
             clearInterval(controller);
         }
 
-    //Construct trivia game.
-        function TriviaGame(quiz,right,wrong){
-            this.numQuestions = quiz;
-            this.rightAnswers = right;
-            this.wrongAnswers = wrong;   
-        }
 
     //Game CONSTRUCTOR METHOD, randomizes answer location.
         function Game(ask, answer, choices){
